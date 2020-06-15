d3ea20e8cbe42e1e1fbf3b6acf5284965da0e59c
Subhasib Ray <subhasibr@infinite.com>
2020-06-15 20:33:54 +0530

    new commit

--- welcome.js
+++ welcome.js
@@ -1,4 +1,10 @@
 function fun()
 {
-    document.write('<h1> hey i did it </h1>')
+    var image = document.createElement('img');
+    var div1 = document.getElementById('div1');
+    var div2 = document.getElementById('id2');
+    image.src = "nature.jpg";
+    var name = prompt("please enter your name");
+    div2.innerHTML = name;
+    div1.appendChild(image);
 }
